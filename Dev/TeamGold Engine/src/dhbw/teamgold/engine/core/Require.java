227eda957b6963256fa22e870e77ae949c5c2fa3
Host127001 <daniel.spaniol@gmx.net>
2016-06-02 07:23:36 +0200

    Added Require Annotation
    * You can now declare Components with Require
    * The GameObjectService will set them
    * Use onInitializeComponents for optional Components

--- Dev/TeamGold Engine/src/dhbw/teamgold/engine/core/Require.java
+++ Dev/TeamGold Engine/src/dhbw/teamgold/engine/core/Require.java
@@ -5,6 +5,35 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation that states that a specific Component is Required for this
+ * Component to work. For example: Assume we have a LocationComponent, an
+ * ImageComponent and an ImageRendererComponent. In order to work the
+ * ImageRendererComponent need the location and the image, these are saved in
+ * their Component. With this annotation you can simply request them like this:
+ * 
+ * <pre>
+ * 	public class ImageRendererComponent {
+ * 		
+ * 		&#64;Require
+ * 		private ImageComponent imageComponent;
+ * 
+ * 		&#64;Require
+ * 		private LocationComponent locationComponent;
+ * 
+ * 		&#64;Override
+ * 		public void onRender(RenderArguments arguments) {
+ * 			float x = locationComponent.getX();
+ * 			float y = locationComponent.getY();
+ * 			Image img = imageComponent.getImage();
+ * 
+ * 			arguments.getGraphics().drawImage(image, x, y);
+ * 		}		
+ * 	}
+ * <pre>
+ * 
+ * @author Daniel Spaniol
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Require {
--- Dev/TeamGold Engine/src/dhbw/teamgold/engine/core/services/GameObjectServiceImpl.java
+++ Dev/TeamGold Engine/src/dhbw/teamgold/engine/core/services/GameObjectServiceImpl.java
@@ -1,9 +1,14 @@
 package dhbw.teamgold.engine.core.services;
 
+import java.lang.reflect.Field;
+
 import dhbw.teamgold.engine.behavior.InitializeArguments;
 import dhbw.teamgold.engine.behavior.services.BehaviorArgumentService;
+import dhbw.teamgold.engine.core.Component;
 import dhbw.teamgold.engine.core.GameObject;
 import dhbw.teamgold.engine.core.Prefab;
+import dhbw.teamgold.engine.core.Require;
+import dhbw.teamgold.engine.core.exception.NoSuchComponentException;
 import dhbw.teamgold.engine.core.prefabs.PlainGameObjectPrefab;
 import dhbw.teamgold.engine.service.Inject;
 
@@ -17,7 +22,7 @@ public class GameObjectServiceImpl implements GameObjectService {
 
 	@Inject
 	BehaviorArgumentService behaviorArgumentService;
-	
+
 	/**
 	 * A Prefab to create a plain GameObject. It is saved here since there is no
 	 * need to create a new instance of it every time we want a GameObject.
@@ -34,8 +39,58 @@ public class GameObjectServiceImpl implements GameObjectService {
 		GameObject object = prefab.getGameObject();
 		InitializeArguments arguments = behaviorArgumentService.createInitializeArguments();
 
-		object.forEachComponent(component -> component.onInitializeComponents(arguments));
+		object.forEachComponent(component -> {
+			provideRequiredComponents(component, object);
+			component.onInitializeComponents(arguments);
+		});
+
 		return object;
+	}
+
+	/**
+	 * The provided Component is scanned for all fields that are Components and
+	 * have the Require-annotation. These are set to the instance of that
+	 * Component-type the provided GameObject has.
+	 * 
+	 * @param component
+	 *            The Component that requires Components.
+	 * @param object
+	 *            The GameObject that has the instances of the requested
+	 *            Component.
+	 * @throws NoSuchComponentException
+	 *             Thrown when the GameObject does not have the instance of a
+	 *             required Component.
+	 */
+	@SuppressWarnings("unchecked")
+	private void provideRequiredComponents(Component component, GameObject object) throws NoSuchComponentException {
+		for (Field field : component.getClass().getDeclaredFields()) {
+			if (fieldIsRequiredComponent(field)) {
+				Class<? extends Component> type = (Class<? extends Component>) field.getType();
+				Component requiredComponent = object.getComponent(type);
+
+				field.setAccessible(true);
+				try {
+					field.set(component, requiredComponent);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Checks if a field is a Component and has the Require-annotation.
+	 * 
+	 * @param field
+	 *            The field to check.
+	 * @return True if the field is a Component with the Require-annotation,
+	 *         false otherwise.
+	 */
+	private boolean fieldIsRequiredComponent(Field field) {
+		boolean isComponent = Component.class.isAssignableFrom(field.getClass());
+		boolean isRequired = field.isAnnotationPresent(Require.class);
+
+		return isRequired && isComponent;
 	}
 
 }
