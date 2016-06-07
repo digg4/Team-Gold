5ef0e5180b5b64b266d69ea21652b532b8e2ee4c
Host127001 <daniel.spaniol@gmx.net>
2016-06-07 16:42:46 +0200

    Added MainMenu

--- Dev/TeamGold Game/src/dhbw/teamgold/game/_TeamGold_Game.java
+++ Dev/TeamGold Game/src/dhbw/teamgold/game/_TeamGold_Game.java
@@ -4,6 +4,8 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
+import dhbw.teamgold.game.mainmenu.MainMenuScene;
+
 /**
  * The Game itself as provided by the Slick2D-engine. It is just a state-machine
  * that delegates the updating and rendering etc to its current state.
@@ -18,6 +20,7 @@ public class _TeamGold_Game extends StateBasedGame {
 
 	@Override
 	public void initStatesList(GameContainer container) throws SlickException {
+		this.addState(new MainMenuScene());
 	}
 
 }
---
+++ Dev/TeamGold Game/src/dhbw/teamgold/game/mainmenu/MainMenuScene.java
@@ -0,0 +1,17 @@
+package dhbw.teamgold.game.mainmenu;
+
+import dhbw.teamgold.engine.core.Scene;
+import dhbw.teamgold.game.SceneIds;
+
+public class MainMenuScene extends Scene {
+
+	public MainMenuScene() {
+		super(SceneIds.MAIN_MENU);
+	}
+
+	@Override
+	protected void initializeGameObjects() {
+		
+	}
+
+}
