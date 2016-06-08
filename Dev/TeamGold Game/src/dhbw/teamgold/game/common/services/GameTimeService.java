5bf1a6b21b25e589170bdb0a7a24ee78e5d60bb2
Host127001 <daniel.spaniol@gmx.net>
2016-06-08 11:17:17 +0200

    Added GameTimeService

---
+++ Dev/TeamGold Game/src/dhbw/teamgold/game/common/services/GameTimeService.java
@@ -0,0 +1,40 @@
+package dhbw.teamgold.game.common.services;
+
+import dhbw.teamgold.engine.service.Service;
+
+public interface GameTimeService extends Service {
+
+	/**
+	 * This method is used to determine if the player is currently able to play
+	 * the game. the player has time left if:
+	 * <ul>
+	 * <li>The player has not started a game yet</li>
+	 * <li>The player has not played for 15 minutes yet</li>
+	 * <li>The last time the player started the game is one hour ago</li>
+	 * </ul>
+	 * 
+	 * @return True when the player has no time left to play the game, false
+	 *         otherwise.
+	 */
+	boolean isTimeOver();
+
+	/**
+	 * Used to determine if the player has started the game and is currently in
+	 * the 15 minute time-frame. This means whenever isTimeTicking returns true
+	 * isTimeOver will return false. But isTimeOver will also return false if
+	 * the game has not started yet. <br/>
+	 * Us this method in order to know when to start the ticking of the time.
+	 * 
+	 * @return True when the player is in the current 15 minute time-frame,
+	 *         false otherwise.
+	 */
+	boolean isTimeTicking();
+
+	/**
+	 * Starts the 15 minute time-frame the player has to play the game. Always
+	 * ensure that the time is not already ticking yet before invoking this
+	 * method.
+	 */
+	void startTimeTicking();
+
+}
---
+++ Dev/TeamGold Game/src/dhbw/teamgold/game/common/services/providers/GameTimeServiceProvider.java
@@ -0,0 +1,62 @@
+package dhbw.teamgold.game.common.services.providers;
+
+import dhbw.teamgold.engine.service.Inject;
+import dhbw.teamgold.engine.service.Provider;
+import dhbw.teamgold.game.common.services.GameStatsPersistenceService;
+import dhbw.teamgold.game.common.services.GameTimeService;
+
+public class GameTimeServiceProvider implements Provider<GameTimeService> {
+
+	@Override
+	public GameTimeService createService() {
+		return new GameTimeServiceImpl();
+	}
+
+	@Override
+	public Class<GameTimeService> getType() {
+		return GameTimeService.class;
+	}
+
+	private static class GameTimeServiceImpl implements GameTimeService {
+
+		private static final int MINUTES_TO_PLAY = 15;
+		private static final int MINUTES_TO_WAIT = 45;
+		
+		private static final long MILISECONDS_TO_PLAY = MINUTES_TO_PLAY * 60 * 1000;
+		private static final long MILISECONDS_TO_WAIT = MINUTES_TO_WAIT * 60 * 1000;
+		
+		@Inject
+		private GameStatsPersistenceService gameStatsPersistenceService;
+		
+		@Override
+		public boolean isTimeOver() {
+			long lastGameStart = gameStatsPersistenceService.getStats().getGameStart();
+			
+			if (waitTimeHasPassed(lastGameStart)) {
+				return false;
+			}
+			
+			return gameTimeHasPassed(lastGameStart);
+		}
+		
+		private boolean waitTimeHasPassed(long lastGameStart) {
+			return System.currentTimeMillis() - lastGameStart > MILISECONDS_TO_WAIT + MILISECONDS_TO_PLAY;
+		}
+		
+		private boolean gameTimeHasPassed(long lastGameStart) {
+			return System.currentTimeMillis() - lastGameStart > MILISECONDS_TO_PLAY;
+		}
+
+		@Override
+		public boolean isTimeTicking() {
+			long lastGameStart = gameStatsPersistenceService.getStats().getGameStart();
+			
+			return !gameTimeHasPassed(lastGameStart);
+		}
+		
+		@Override
+		public void startTimeTicking() {
+			gameStatsPersistenceService.getStats().setGameStart(System.currentTimeMillis());
+		}
+	}
+}
