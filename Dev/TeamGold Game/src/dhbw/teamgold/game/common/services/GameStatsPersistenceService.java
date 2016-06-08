639db84b8cc619bd5b3dc1111d8e724dc8342233
Host127001 <daniel.spaniol@gmx.net>
2016-06-08 10:09:11 +0200

    Added some JavaDoc

--- Dev/TeamGold Game/src/dhbw/teamgold/game/common/services/GameStatsPersistenceService.java
+++ Dev/TeamGold Game/src/dhbw/teamgold/game/common/services/GameStatsPersistenceService.java
@@ -4,12 +4,24 @@ import dhbw.teamgold.engine.service.Service;
 import dhbw.teamgold.game.common.PersistentGameStats;
 
 /**
- * @author Daniel
+ * This Service has the purpose to load and save the GameStats. This should
+ * mainly be a helper-Service for other services.
+ * 
+ * @author Daniel Spaniol
  */
 public interface GameStatsPersistenceService extends Service {
 
+	/**
+	 * Loads the stats from the savegame-file if there is one, otherwise new
+	 * stats are created.
+	 * 
+	 * @return The stats.
+	 */
 	PersistentGameStats getStats();
-	
+
+	/**
+	 * Saves the stats in the savegame-file.
+	 */
 	void saveStats();
-	
+
 }
