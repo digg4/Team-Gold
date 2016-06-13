22089aae5e49dd0034bf31420181851c6c888156
Host127001 <daniel.spaniol@gmx.net>
2016-06-13 12:03:24 +0200

    Externalized more strings

--- Dev/TeamGold Game/src/dhbw/teamgold/game/timeoverscreen/TimeOverMenuScene.java
+++ Dev/TeamGold Game/src/dhbw/teamgold/game/timeoverscreen/TimeOverMenuScene.java
@@ -3,6 +3,7 @@ package dhbw.teamgold.game.timeoverscreen;
 import org.newdawn.slick.geom.Rectangle;
 
 import dhbw.teamgold.engine.core.Scene;
+import dhbw.teamgold.game.Messages;
 import dhbw.teamgold.game.SceneIds;
 import dhbw.teamgold.game.common.prefabs.SwitchSceneButtonPrefab;
 import dhbw.teamgold.game.common.prefabs.TextDisplayPrefab;
@@ -15,8 +16,9 @@ public class TimeOverMenuScene extends Scene {
 
 	@Override
 	protected void initializeGameObjects() {
-		this.addGameObject(new TextDisplayPrefab("The Play Time is Over"));
-		this.addGameObject(new SwitchSceneButtonPrefab(new Rectangle(0.426f, 0.75f, 0.15f, 0.05f),"Back to Main Menu", SceneIds.MAIN_MENU));
+		this.addGameObject(new TextDisplayPrefab(Messages.getString("TimeOverMenu.TimeOver")));
+		this.addGameObject(new SwitchSceneButtonPrefab(new Rectangle(0.426f, 0.75f, 0.15f, 0.05f),
+				Messages.getString("TimeOverMenu.Back"), SceneIds.MAIN_MENU));
 	}
 
 }
