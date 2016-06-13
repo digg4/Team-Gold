d464b4b3d5dbf71cef79e558e33784d34763c904
Host127001 <daniel.spaniol@gmx.net>
2016-06-13 10:23:32 +0200

    Minor refactors
    renamed ScorePrefab to ScoreDisplayPrefab

--- Dev/TeamGold Game/src/dhbw/teamgold/game/pausemenu/PauseMenuScene.java
+++ Dev/TeamGold Game/src/dhbw/teamgold/game/pausemenu/PauseMenuScene.java
@@ -5,7 +5,7 @@ import org.newdawn.slick.geom.Rectangle;
 import dhbw.teamgold.engine.core.Scene;
 import dhbw.teamgold.game.SceneIds;
 import dhbw.teamgold.game.common.prefabs.SwitchSceneButtonPrefab;
-import dhbw.teamgold.game.common.prefabs.ScorePrefab;
+import dhbw.teamgold.game.common.prefabs.ScoreDisplayPrefab;
 import dhbw.teamgold.game.common.prefabs.GameTimeDisplayPrefab;
 import dhbw.teamgold.game.pausemenu.prefabs.LivesPrefab;
 import dhbw.teamgold.game.pausemenu.prefabs.PauseMenuBackgroundPrefab;
@@ -22,7 +22,7 @@ public class PauseMenuScene extends Scene {
 		this.addGameObject(new PauseMenuBackgroundPrefab());
 		this.addGameObject(new GameTimeDisplayPrefab());
 		this.addGameObject(new LivesPrefab());
-		this.addGameObject(new ScorePrefab());
+		this.addGameObject(new ScoreDisplayPrefab());
 		
 		this.addGameObject(new ResumeButtonPrefab());
 		this.addGameObject(new SwitchSceneButtonPrefab(new Rectangle(0.45f,0.46f,0.15f,0.05f), "Options", SceneIds.STATS_SCREEN));
