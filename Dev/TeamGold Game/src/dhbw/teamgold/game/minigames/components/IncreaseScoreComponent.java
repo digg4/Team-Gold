81d7c5c8b9e971ed9f79b15fdb417e5777a40f11
Host127001 <daniel.spaniol@gmx.net>
2016-06-16 08:03:52 +0200

    Highscore will be updated

--- Dev/TeamGold Game/src/dhbw/teamgold/game/minigames/components/IncreaseScoreComponent.java
+++ Dev/TeamGold Game/src/dhbw/teamgold/game/minigames/components/IncreaseScoreComponent.java
@@ -4,17 +4,23 @@ import dhbw.teamgold.engine.behavior.InitializeArguments;
 import dhbw.teamgold.engine.core.Component;
 import dhbw.teamgold.engine.service.Services;
 import dhbw.teamgold.game.common.services.GameStatsService;
+import dhbw.teamgold.game.common.services.HighscoreService;
 
 public class IncreaseScoreComponent extends Component {
 
 	private static final int SCORE_PER_LEVEL = 1000;
 	
 	private GameStatsService stats = Services.get(GameStatsService.class);
+	private HighscoreService highscore = Services.get(HighscoreService.class);
 	
 	@Override
 	public void onInitializeGameObject(InitializeArguments arguments) {
-		int currentScore = stats.getStats().getScore();
-		stats.getStats().setScore(currentScore + SCORE_PER_LEVEL);
+		int newScore = stats.getStats().getScore() + SCORE_PER_LEVEL;
+		stats.getStats().setScore(newScore);
+		
+		if (highscore.isNewHighscore(newScore)) {
+			highscore.setHighscore(newScore);
+		}
 	}
 	
 }
